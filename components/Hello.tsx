0176cbbe9b0e28ccd07bfb1684efe830cc2b697b
sleepyfox97 <h1649bot@gmail.com>
2024-09-08 16:04:58 +0900

    first commit

---
+++ components/Hello.tsx
@@ -0,0 +1,13 @@
+const Hello = () => {
+    const onClick = () => {
+        alert('hello');
+    }
+    const text = 'Hello React'
+    return (
+        <div onClick={onClick}>
+            {text}
+        </div>
+    )
+}
+
+export default Hello
